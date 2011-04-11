2e7692b0ac462aa0bc64cb8a4dd345ca7b51dbf9
Reece Hart <reece@invitae.com>
2011-04-10 21:40:44 -0700

    added *.orig to cleanest rule

--- lib/Bio/HGVS/Variant.pm
+++ lib/Bio/HGVS/Variant.pm
@@ -24,22 +24,23 @@ use Carp::Assert;
 use Data::Dumper;
 
 use Bio::HGVS::Errors;
-use Bio::HGVS::VariantCoordinate;
+use Bio::HGVS::Location;
 
 use overload
   '""' => \&stringify;
 
 # Instance variables:
 use Mouse;									# must be after B:GC:Errors!
-has 'end' 	=> ( is => 'rw' );
-has 'intron_offset' => ( is => 'rw', default => 0 );
+has 'loc' 	=> ( is => 'rw', isa => 'Bio::HGVS::Location' );
 has 'name' 	=> ( is => 'rw' );
 has 'op' 	=> ( is => 'rw' );
 has 'post' 	=> ( is => 'rw' );
 has 'pre' 	=> ( is => 'rw' );
 has 'ref'  	=> ( is => 'rw' );
-has 'start' => ( is => 'rw' );
 has 'type' 	=> ( is => 'rw', required => 1 );
+no Moose;
+__PACKAGE__->meta->make_immutable;
+
 
 
 =head1 METHODS
@@ -57,45 +58,15 @@ insertions, the length is 0.
 
 sub len {
   my ($self) = @_;
-  return $self->end - $self->start + 1;
-}
-
-
-
-############################################################################
-
-=head2 ->pos()
-
-returns the position of the variant as a simple position, range, or
-intron-based coordinate
-
-=cut
-
-sub pos {
-  my ($self) = @_;
-  my $range_delim = '_';
-
-  (defined $self->start)
-	|| throw Bio::HGVS::Error("Range doesn't have a start!");
-
-  if (defined $self->intron_offset and $self->intron_offset != 0) {
-	return sprintf('%s%+d', $self->start, $self->intron_offset);
-  }
-
-  if (defined $self->end and $self->start != $self->end) {
-	(defined $self->intron_offset and $self->intron_offset != 0) 
-	return sprintf('%s%s%s', $self->start, $range_delim, $self->end);
-  }
-
-  return $self->start;
+  return $self->loc->len;
 }
 
 sub var {
   my ($self) = @_;
   if ($self->type eq 'p') {
-	return sprintf('%s%s%s', $self->pre, $self->pos, $self->post),
+	return sprintf('%s%s%s', $self->pre, $self->loc, $self->post),
   }
-  return sprintf('%s%s>%s', $self->pos, $self->pre, $self->post);
+  return sprintf('%s%s>%s', $self->loc, $self->pre, $self->post);
 }
 
 sub stringify {
--- t/Variant.pl
+++ t/Variant.pl
@@ -3,36 +3,44 @@
 use strict;
 use warnings;
 
-use Test::More tests => 1;
+use Test::More tests => 2;
 
 use FindBin;
-use lib "$FindBin::RealBin/../../..";
+use lib "$FindBin::RealBin/../lib";
 use Bio::HGVS::Variant;
+use Bio::HGVS::Position;
+use Bio::HGVS::Range;
+
 
 my @tests = (
   {
-	end => 57,
 	name => 'test',
 	post => 'GT',
 	pre => 'AC',
 	ref => 'NM_00123.4',
-	start => 56,
 	type => 'g',
+	loc => Bio::HGVS::Range->new( 
+	  start => Bio::HGVS::Position->new( position => 56 ),
+	  end   => Bio::HGVS::Position->new( position => 57 )
+	 ),
   },
   {
 	name => 'test2',
 	post => 'T',
 	pre => 'A',
 	ref => 'NM_00123.4',
-	start => 56,
-	intron_offset => '+3',
 	type => 'c',
+	loc => Bio::HGVS::Range->new(
+	  start => Bio::HGVS::Position->new( position => 56, intron_offset => 3 ),
+	 ),
   },
  );
 
 
 foreach my $test (@tests) {
   my $v = Bio::HGVS::Variant->new( %$test );
+  print("* $v\n");
+  print("loc = ", $v->loc, "\n");
   my @keys = sort keys %$test;
   my @failed = grep { $v->$_ ne $test->{$_} } @keys;
   ok( $#failed == -1, sprintf('%d keys: {%s}; %d failed: {%s}',
