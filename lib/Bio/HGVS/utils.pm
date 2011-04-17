ed2f3c397d2a06ac381cce326cfa01d831cfb86f
'Reece Hart <reecehart@gmail.com>
2011-04-17 23:47:53 +0000

    added simple string AA 1-to-3 and 3-to-1 routines

--- lib/Bio/HGVS/utils.pm
+++ lib/Bio/HGVS/utils.pm
@@ -1,15 +1,33 @@
 package Bio::HGVS::utils;
 
 use base 'Exporter';
-our @EXPORT_OK = qw(aa3 aa3_re);
+our @EXPORT_OK = qw(aa1to3 aa3to1 aa3 aa3_re);
 
 
-my @aa3 = qw(Ala Arg Asn Asp Cys Gln Glu Gly His Ile Leu
-			  Lys Met Phe Pro Ser Thr Trp Tyr Val);
+my %AA3to1 = (
+  'Ala' => 'A', 'Asx' => 'B', 'Cys' => 'C', 'Asp' => 'D',
+  'Glu' => 'E', 'Phe' => 'F', 'Gly' => 'G', 'His' => 'H',
+  'Ile' => 'I', 'Lys' => 'K', 'Leu' => 'L', 'Met' => 'M',
+  'Asn' => 'N', 'Pro' => 'P', 'Gln' => 'Q', 'Arg' => 'R',
+  'Ser' => 'S', 'Thr' => 'T', 'Val' => 'V', 'Trp' => 'W',
+  'Xaa' => 'X', 'Tyr' => 'Y', 'Glx' => 'Z', 'Ter' => '*',
+  'Sel' => 'U'
+ );
+my %AA1to3 = map { $AA3to1{$_} => $_ } keys %AA3to1;
+
+my @aa3 = sort keys %AA3to1;
+my @aa1 = sort keys %AA1to3;
 
 my $aa3_re = join('|',@aa3);
 $aa3_re = qr/$aa3_re/;
 
+sub aa1to3 {
+  join('', map {$AA1to3{$_}||'???'} $_[0] =~ m/(.)/g);
+}
+
+sub aa3to1 {
+  join('', map {$AA3to1{$_}||'?'} $_[0] =~ m/(...)/g);
+}
 
 1;
 
