1b75f41ff01983ffa8122ec92128cf111bf75818
Reece Hart <reece@invitae.com>
2011-12-16 17:50:27 +0000

    output ensembl version on startup; updated doc; removed hardcoded path to ensembl modules

--- lib/Bio/HGVS.pm
+++ lib/Bio/HGVS.pm
@@ -1,23 +1,15 @@
 package Bio::HGVS;
 
+
 use Cwd 'abs_path';
 use File::Basename qw(dirname);
 
+# "extra" packages, if any, are in <installation root>/ext/
 our $ROOT;
-our $E_BASE;
 BEGIN {
   $ROOT = dirname(dirname(dirname(abs_path(__FILE__))));
-  $E_BASE = '/locus/opt/ensembl';
 }
 
 use lib  "$ROOT/ext/lib/perl5";
-
-use lib map {"$E_BASE/$_"} (qw(
-	bioperl-1.2.3
-    61/bioperl-live
-    61/ensembl/modules
-    61/ensembl-variation/modules
-  ));
-
 
 1;
