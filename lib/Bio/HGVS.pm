663aa58b18731a10553b12b27a0599903ce8c209
Jon Sorenson <drkeoni@users.noreply.github.com>
2012-02-01 00:58:25 +0000

    adding some notes about what to think about when we deploy from tip

--- lib/Bio/HGVS.pm
+++ lib/Bio/HGVS.pm
@@ -10,6 +10,11 @@ BEGIN {
   $ROOT = dirname(dirname(dirname(abs_path(__FILE__))));
 }
 
+#
+# n.b. when we deploy from tip we'll need to source /locus/opt/ensembl/config
+# before running code that depends on this module
+#
+
 use lib  "$ROOT/ext/lib/perl5";
 
 1;
